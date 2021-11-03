5dac9d64092ffacee653b63d5c39986b9067efcd
aarv1e <87652403+aarv1e@users.noreply.github.com>
2021-11-03 15:54:35 +0530

    Update rpsGame.py

--- rpsGame.py
+++ rpsGame.py
@@ -1,7 +1,7 @@
 import random
 
-CHOICES = ["rock","paper","scissor"]
-def gameWin (comp , you):
+CHOICES = ["rock","paper","scissor"]           #the list of valid choices
+def gameWin (comp , you):                      #creating a method to determine the winner between comp and user
     if comp == you:
         print("The game is a draw")
     elif comp == "scissor":
@@ -20,18 +20,18 @@ def gameWin (comp , you):
         elif you == "scissor":
             print("The computer won")
 
-while True:
+while True:                                                                           #initialising an endless loop 
     print("Make your throw")
     user_choice = input("Type rock, paper or scissor: ")
     if user_choice.lower() in CHOICES:
-        computer_choice = random.choice(CHOICES)
+        computer_choice = random.choice(CHOICES)                                      #using the 'choice' method of the 'random' module to determine the comp choice
         print(f"You threw '{user_choice}', the computer threw '{computer_choice}'")
     else:
         print(f"{user_choice} isn't a valid throw")
 
-    gameWin(computer_choice,user_choice.lower())
+    gameWin(computer_choice,user_choice.lower())                                      #calling the 'gameWin' function to determine the winner amongst comp and user
 
-    again = input("Want to play again? (y/n): ")
+    again = input("Want to play again? (y/n): ")                                      #asking the user whether he wants to continue the game
     if again.lower() == "n":
         break
 
